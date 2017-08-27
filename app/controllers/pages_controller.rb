1766b5b08f1871609e4f4f21684ad607ede6657c
Jjavier Silva <javier.eduardo.sp@gmail.com>
2017-08-27 00:12:54 -0300

    estructura backend

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,22 +6,21 @@ class PagesController < ApplicationController
     @niños = params[:ninos].to_i
     @mujeres = params[:mujeres].to_i
     @hombres = params[:hombres].to_i
-    @suma_invitados = @niños + @mujeres + @hombres
-
     @carne = params[:carne]
+
+    @suma_invitados = @niños + @mujeres + @hombres
     @carne_total = @niños*0.2 + @mujeres*0.25 + @hombres*0.3
 
-    if @carne = "Lomo Vetado"
+
+    if @carne == "Lomo Vetado"
         @precio_carne = @carne_total*9500
-    elsif @carne = "Lomo Liso"
+
+    elsif @carne == "Lomo Liso"
         @precio_carne = @carne_total*8500
-    elsif @carne = "Huachalomo"
+
+    elsif @carne == "Huachalomo"
         @precio_carne = @carne_total*4500
     end
-
-    @cerveza = params[:cerveza]
-    @cerveza_total = @niños*0 + @mujeres*0.8 + @hombres*1.5
-
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'pages/form'
+  post 'pages/calculo'
   get 'pages/calculo'
   root 'pages#form'
-  #post 'pages', to: 'pages#calculo'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
